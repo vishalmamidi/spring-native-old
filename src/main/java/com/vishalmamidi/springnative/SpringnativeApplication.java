1ea9fe774056df93be803fed61f4843ab2b394c7
vishalmamidi1 <vishal.mamidi@valuemomentum.com>
2022-08-17 03:40:40 +0530

    chagned response

--- src/main/java/com/vishalmamidi/springnative/SpringnativeApplication.java
+++ src/main/java/com/vishalmamidi/springnative/SpringnativeApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @SpringBootApplication
@@ -23,10 +26,18 @@ public class SpringnativeApplication {
 	}
 
     @GetMapping("/")
-    public String index() {
-        return "Time between start and ApplicationReadyEvent: "
-                + Duration.between(startTime, readyTime).toMillis()
+    public ResponseEntity<Map<String, String>> homePage() {
+
+        String staratup = Duration.between(startTime, readyTime).toMillis()
                 + "ms";
+
+        Map <String, String> response = new HashMap<String, String>();
+        response.put("message", "Application Started in "+ staratup);
+        response.put("timestamp", java.time.LocalDateTime.now().toString());
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(response);
     }
 
     @EventListener(ApplicationReadyEvent.class)
